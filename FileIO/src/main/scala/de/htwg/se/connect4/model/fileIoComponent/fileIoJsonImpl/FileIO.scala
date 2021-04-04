c4a29b2355814c0c87b61d96e6a934da1a4fbd53
JoshuaHelmle <joshua.helmle@htwg-konstanz.de>
2021-04-04 19:01:26 +0200

    refactored board and FileIO into modules

---
+++ FileIO/src/main/scala/de/htwg/se/connect4/model/fileIoComponent/fileIoJsonImpl/FileIO.scala
@@ -0,0 +1,124 @@
+package main.model.fileIoJsonImpl
+
+import com.google.inject.Guice
+import de.htwg.se.connect4.controller.controllerComponent.controllerBaseImpl.State
+import de.htwg.se.connect4.model.boardComponent.BoardInterface
+import main.model.FileIoInterface
+import com.google.inject.Guice
+import de.htwg.se.connect4.Connect4Module
+import de.htwg.se.connect4.controller.controllerComponent.controllerBaseImpl.State
+import de.htwg.se.connect4.model.boardComponent.boardBaseImpl.BoardSizeStrategy
+import de.htwg.se.connect4.model.boardComponent.boardBaseImpl.Color.Color
+import de.htwg.se.connect4.model.boardComponent.{BoardInterface, CellInterface}
+import de.htwg.se.connect4.model.fileIoComponent.FileIoInterface
+import de.htwg.se.connect4.model.playerComponent.Player
+import play.api.libs.json._
+
+import scala.io.Source
+
+import scala.io.Source
+import scala.io.Source
+
+class FileIO extends FileIoInterface {
+
+  override def load: (BoardInterface, State) = {
+    val source: String = Source.fromFile("board.json").getLines.mkString
+    val json: JsValue = Json.parse(source)
+
+    val sizeOfRows = (json \ "board" \ "row").get.toString.toInt
+    val sizeOfCols = (json \ "board" \ "col").get.toString.toInt
+
+    var board = BoardSizeStrategy.execute(sizeOfRows, sizeOfCols)
+
+    val injector = {
+      Guice.createInjector(new Connect4Module)
+    }
+
+    for (index <- 0 until sizeOfRows * sizeOfCols) {
+      val row = (json \ "board" \ "cells" \\ "row") (index).as[Int]
+      val col = (json \ "board" \ "cells" \\ "col") (index).as[Int]
+      val cell = (json \\ "cell") (index)
+      val isSet = (cell \ "isSet").as[Boolean]
+      val color = (cell \ "color" \ "color").as[Color]
+
+      board = board.set(row, col, color, isSet)
+
+    }
+
+    val currentPlayerIndex = (json \ "currentPlayerIndex").get.toString().toInt
+    var players: List[Player] = List()
+
+    for (index <- 0 until 2) {
+      val name = (json \ "players" \\ "name") (index).as[String]
+      val piecesLeft = (json \ "players" \\ "piecesLeft") (index).as[Int]
+      val playerColor = (json \ "players") (index)("color")
+      val color = (playerColor \ "color").as[Color]
+
+
+      val player = new Player(name, color, piecesLeft)
+      players = players ::: List(player)
+
+    }
+
+    val state = (json \ "state").as[String]
+
+    val stateToLoad = new State(currentPlayerIndex, players, state)
+
+    (board, stateToLoad)
+
+  }
+
+  override def save(board: BoardInterface, state: State): Unit = {
+    import java.io._
+    val pw = new PrintWriter(new File("board.json"))
+    pw.write(Json.prettyPrint(gridToJson(board, state)))
+    pw.close
+  }
+
+
+  def gridToJson(board: BoardInterface, state: State) = {
+    Json.obj(
+      "currentPlayerIndex" -> JsNumber(state.currentPlayerIndex),
+      "state" -> JsString(state.state),
+      "players" -> Json.toJson(
+        for {
+          index <- state.players.indices
+
+        } yield {
+          Json.obj(
+            "name" -> state.players(index).playerName,
+            "color" -> state.players(index).color,
+            "piecesLeft" -> state.players(index).piecesLeft,
+          )
+        }
+
+      ),
+      "board" -> Json.obj(
+        "row" -> JsNumber(board.sizeOfRows),
+        "col" -> JsNumber(board.sizeOfCols),
+        "cells" -> Json.toJson(
+          for {
+            row <- 0 until board.sizeOfRows
+            col <- 0 until board.sizeOfCols
+          } yield {
+            Json.obj(
+              "row" -> row,
+              "col" -> col,
+              "cell" -> Json.toJson(board.cell(row, col))
+            )
+          }
+        )
+      )
+    )
+  }
+
+  implicit val cellWrites = new Writes[CellInterface] {
+    def writes(cell: CellInterface) = Json.obj(
+      "isSet" -> cell.isSet,
+      "color" -> cell.color
+
+    )
+  }
+}
+
+
---
+++ FileIO/src/main/scala/de/htwg/se/connect4/model/fileIoComponent/fileIoXmlImpl/FileIO.scala
@@ -0,0 +1,99 @@
+package main.model.fileIoXmlImpl
+
+class FileIO extends FileIoInterface {
+
+
+  override def load: (BoardInterface, State) = {
+    val file = scala.xml.XML.loadFile("board.xml")
+    val rowAttr = file \\ "game"  \\ "board" \ "@row"
+    val colAttr = file \\ "game" \\ "board" \  "@col"
+
+    val sizeOfRows = rowAttr.text.toInt
+    val sizeOfCols = colAttr.text.toInt
+
+    var board = BoardSizeStrategy.execute(sizeOfRows, sizeOfCols)
+
+    val cellNodes = file \\ "cell"
+    for (cell <- cellNodes) {
+      val row: Int = (cell \ "@row").text.toInt
+      val col: Int = (cell \ "@col").text.toInt
+      val colorAttr: String = (cell \ "@color").text
+      val color: Color = Color.toEnum(colorAttr)
+      val isSet: Boolean = (cell \ "@isSet").text.toBoolean
+      board = board.set(row, col, color, isSet)
+    }
+
+    val currentPlayerIndex: Int = (file \\ "game" \\ "@currentPlayerIndex").text.toInt
+    val stateString: String = (file \\ "game" \\ "@stateString").text
+    var players: List[Player] = List()
+
+    val playerNodes = (file \\ "player")
+    for (player <- playerNodes) {
+      val name: String = (player \ "@name").text
+      val colorAttr: String = (player \ "@color").text
+      val color: Color = Color.toEnum(colorAttr)
+      val piecesLeft: Int = (player \ "@piecesLeft").text.toInt
+
+      val playerObject = new Player(name, color, piecesLeft)
+      players = players ::: List(playerObject)
+
+    }
+
+    val state = new State(currentPlayerIndex, players, stateString)
+
+    (board, state)
+
+  }
+
+  override def save(board: BoardInterface, state: State): Unit = saveString(board, state)
+
+  def saveString(board: BoardInterface, state: State): Unit = {
+    import java.io._
+    val pw = new PrintWriter(new File("board.xml"))
+    val prettyPrinter = new PrettyPrinter(200, 2)
+    val xml = prettyPrinter.format(boardToXml(board, state))
+    pw.write(xml)
+    pw.close
+
+  }
+
+  def boardToXml(board: BoardInterface, state: State) = {
+    <game>
+      <currentPlayerIndex currentPlayerIndex =
+        {state.currentPlayerIndex.toString}>
+      </currentPlayerIndex>
+      <stateString stateString =
+        {state.state.toString}>
+      </stateString>
+
+      <players>
+        {for {
+        index <- state.players.indices
+
+      } yield playerToXml(state, index)}
+      </players>
+
+      <board row={ board.sizeOfRows.toString } col = {board.sizeOfCols.toString} >
+        {
+        for {
+          row <- 0 until board.sizeOfRows
+          col <- 0 until board.sizeOfCols
+        } yield cellToXml(board, row, col)
+        }
+      </board>
+    </game>
+
+
+  }
+
+  def cellToXml(board: BoardInterface, row: Int, col: Int) = {
+    <cell row={row.toString} col={col.toString} isSet={board.cell(row, col).isSet.toString}
+      color={board.cell(row, col).color.toString}>
+    </cell>
+  }
+
+  def playerToXml(state: State, i: Int) = {
+    <player name={state.players(i).playerName.toString} piecesLeft={state.players(i).piecesLeft.toString} color={state.players(i).color.toString}>
+    </player>
+  }
+}
